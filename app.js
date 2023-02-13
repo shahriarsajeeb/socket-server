dca90f8844cb1b9539219619612f0f09ab1fbf8b
Shahriar Sajeeb <shahriarsajeeb@Shahriars-MacBook-Air.local>
2023-02-14 01:34:53 +0600

    final commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const socketIO = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 80;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (receiverId) => {
+  return users.find((user) => user.userId === receiverId);
+};
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log(`a user connected.`);
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text,images }) => {
+    console.log(text)
+    const user = getUser(receiverId);
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      text,
+      images
+    });
+  });
+
+  //update and get last message
+  socket.on("updateLatestMessage", ({latestMessage,latestMessageId}) => {
+    io.emit("getLatestMessage",{
+        latestMessage,
+        latestMessageId,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log(`a user disconnected!`);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+
+});
+
+server.listen(PORT, () => {
+  console.log(`server is working on port ${PORT}`);
+});
